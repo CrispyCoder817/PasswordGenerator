a5892504dccb6345696737bdf0e2bf4e23f28940
Brandon G.M <brandongmerritt@gmail.com>
2021-02-13 14:37:46 -0500

    added files to repo

---
+++ Develop/script.js
@@ -0,0 +1,30 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+includelowercaseNumber.
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+  let passwordLength = prompt("Please enter the number of characters you want for your new passower.";)
+
+}
+const characterAmountRange = document.getElementById(
+  ('characterAmountRange'))
+  console.passwordLength
+
+//   const characterAmountRange = document.getElementById(
+//     ('characterAmountRange')
+
+//     characterAmountRange.addEventListener('input',syncCharacterAmount)
+//     characterAmountNumber.addEventListener('input',syncCharacterAmount)
+ 
+
+    // split = give it a string.split
